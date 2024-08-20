6c51f29a0d0d7f097139a9a82097bf690b73782c
pingguomc <141195321+pingguomc@users.noreply.github.com>
2024-08-20 16:26:36 +0800

    none

---
+++ src/main/java/top/redstarmc/api/Yggdrasil/Main.java
@@ -0,0 +1,16 @@
+package top.redstarmc.api.Yggdrasil;
+
+import org.slf4j.Logger;
+
+public class Main {
+    private Logger logger;
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println("Starting Main ...");
+        main.logger.info("");
+    }
+}
